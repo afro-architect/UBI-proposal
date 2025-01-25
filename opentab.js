40747f8ed0d9faa93f741e6445de2bb801ac94c8
afro-architect <157778913+afro-architect@users.noreply.github.com>
2025-01-25 09:54:03 -0800

    Update opentab.js

--- opentab.js
+++ opentab.js
@@ -1,15 +1,18 @@
-function opentab(evt, tabName, displayTabName) {
+function opentab(evt, tabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+    tabcontent[i].classList.remove("active");
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].classList.remove("active");
   }
-  document.getElementById(tabName).style.display = "block";
-  document.getElementById(displayTabName).style.display = "block";
-  evt.currentTarget.className += " active";
-  }
-  
+  document.getElementById(tabName).classList.add("active");
+  evt.currentTarget.classList.add("active");
+}
+
+// Set the first tab as active by default
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".tablinks").click();
+});
