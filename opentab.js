3920294ee7ba57d69d5a75cab13d7101bb8a77d3
afro-architect <157778913+afro-architect@users.noreply.github.com>
2025-01-25 10:02:20 -0800

    Update opentab.js

--- opentab.js
+++ opentab.js
@@ -11,3 +11,6 @@ function opentab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".tablinks").click();
+});
